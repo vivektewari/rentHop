7d6213e3928b97239c567c022e64a26779a0bc48
vivektewari2000 <vivektewari2000@gmail.com>
2017-03-27 01:40:55 +0530

    atleast cost converging in 100 iteration

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 def sigmoid(x):return 1/(1+np.exp(x)**-1)
 def sigDeriv(x):return sigmoid(x)*(1-sigmoid(x))
 class layer(object):
@@ -26,14 +27,25 @@ class neuralNetworks(object):
 
         self.modelOutput=input
     def backwardPropagation(self):
-        self.cost=np.sum(self.actualOutput*(np.log(self.modelOutput)/np.dot(self.modelOutput,np.ones(shape=(3,1)))))
-        cofChange=0.3*np.dot(np.transpose(self.input),(self.actualOutput/self.modelOutput)*self.funcGradient(self.layerOutput[-1]))
+        y=(self.modelOutput.shape[0])
+        x=self.actualOutput*(np.log((self.modelOutput)/np.dot(self.modelOutput,np.ones(shape=(3,1)))))
+        self.cost=(-1.0/(self.modelOutput.shape[0]))*np.sum(self.actualOutput*(np.log((self.modelOutput)/np.dot(self.modelOutput,np.ones(shape=(3,1))))))
+        cofChange=0.3*(-1.0/(self.modelOutput.shape[0]))*np.dot(np.transpose(self.input),(self.actualOutput/self.modelOutput)*self.funcGradient(self.layerOutput[-1]))
         self.layers[0].cofficient-=cofChange
     def findEstimates(self):
-        for i in range(1,20):
+        for i in range(1,200):
             self.feedForward()
             self.backwardPropagation()
             print self.cost
+    def predict(self,test,output):
+        summit = test[['listing_id']]
+        summit['key'] = range(1, len(summit) + 1)
+        t = pd.DataFrame(self.modelOutput, columns=['high','medium','low'], index=summit.index)
+        t['key'] = range(1, len(t) + 1)
+        final = summit.merge(t, on='key', how='left')
+        final = final[['listing_id', 'high', 'medium', 'low']]
+        final = final.set_index('listing_id')
+        final.to_csv(output, sep=',')
 
 
 
