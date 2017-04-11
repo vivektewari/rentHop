39f59e05b5e1bfe59fe7d88ec230e68c7150cfa0
vivektewari2000 <vivektewari2000@gmail.com>
2017-04-11 16:22:49 +0530

    wip ,,trying to minimize with bfsg

--- wholeSum.py
+++ wholeSum.py
@@ -1,4 +1,5 @@
 from multiprocessing import Pool,Process,cpu_count,Manager
+from scipy.optimize import minimize
 import time
 import copy
 import json,csv
@@ -35,7 +36,7 @@ def nnOutput(train,test,element,var,analyse=False):
     print "starting to run for subset"
     if analyse:analyseData=train.copy(deep=True)
     rawTransformed=train
-    secondLayer=5
+    secondLayer=8
     d=nn(listOfMatrix=[np.random.rand(len(var),secondLayer),np.random.rand(secondLayer,3)],input=rawTransformed[var].as_matrix(),output=rawTransformed[['high','medium','low']].as_matrix(),func=sigmoid,funcGradient=sigDeriv,variables=var,iteration=500)
     d.findEstimates()
     print d.cost,d.input.shape[0]
@@ -48,9 +49,9 @@ def nnOutput(train,test,element,var,analyse=False):
     return prediction,d.cost,d.input.shape[0]
 
 
-raw=pd.read_json('input\\train.json')
+raw=pd.read_json('input\\train.json')[0:1000]
 
-test=pd.read_json('input\\test.json')
+test=pd.read_json('input\\test.json')[0:1000]
 r=copy.deepcopy(raw)
 t=copy.deepcopy(test)
 
@@ -83,22 +84,42 @@ r=r[r['price']<3100]
 #     pred, cost2, row2=nnOutput(rawData[r['price']>=split],tester[t['price']>=split],1,var)
 #     print (cost1*row1+cost2*row2)/31720
 #from boost import nnOutput
-var=var +['high_manager','low_manager']
+var=var +['listing_id']
 r=r[rawData['low_manager']<=0.7303]
 rawData=rawData[rawData['low_manager']<=0.7303]
-
+rawData['listing_id']=rawData['listing_id'].mean()/(rawData['listing_id'].max()-rawData['listing_id'].min())
 tester=rawData
-rindexes=rawData.query('low_manager <=0.5084').index
-tindexes=tester.query('low_manager <= 0.5084').index
-# rawData1=rawData.loc[rindexes,:]
-# rawData2=rawData.drop(rindexes)
+# rindexes=rawData.query('low_manager <=0.5084').index
+# tindexes=tester.query('low_manager <= 0.5084').index
+# # rawData1=rawData.loc[rindexes,:]
+# # rawData2=rawData.drop(rindexes)
+#
+# pred1= nnOutput(rawData.loc[rindexes,:], tester.loc[tindexes,:], 1, var,True)
+# pred2= nnOutput(rawData.drop(rindexes), tester.drop(tindexes), 1, var,True)
+# pred=pred1.append(pred2)
+# pred.update(r)
+# pred=pred[var+['high','medium','low','cost']]
+# pred.to_csv('output\\highnalysis.csv', sep=',')
+data=rawData[var].as_matrix()
+def fun(p):
+    a=(sigmoid(np.dot(data,np.matrix(p.reshape(len(var),2)))))
+    b=1-np.sum(a,axis=1)
+    prob=np.matrix(np.hstack((a,b)))
+    logp=np.log(prob)
+    t=np.sum(np.multiply(rawData[['high','medium','low']].as_matrix(),prob))
+    return t
+f=fun(np.random.rand(len(var)*2))
+cons = ({'type': 'ineq', 'fun': lambda p:  np.squeeze(np.array(np.ones(shape=(data.shape[0],1))-np.sum(sigmoid(np.dot(data,np.matrix(p.reshape(len(var),2)))),axis=1)))},
+         {'type': 'ineq', 'fun':lambda p: np.squeeze(np.array(sigmoid(np.dot(data, p[0:len(var)].reshape(len(var),1)))))},
+        {'type': 'ineq', 'fun':lambda p: np.squeeze(np.array(sigmoid(np.dot(data, p[len(var):2*len(var)].reshape(len(var),1)))))})
 
-pred1= nnOutput(rawData.loc[rindexes,:], tester.loc[tindexes,:], 1, var,True)
-pred2= nnOutput(rawData.drop(rindexes), tester.drop(tindexes), 1, var,True)
-pred=pred1.append(pred2)
-pred.update(r)
-pred=pred[var+['high','medium','low','cost']]
-pred.to_csv('output\\highnalysis.csv', sep=',')
+# t= np.random.rand(len(var),3)
+# r=np.sum(sigmoid(np.dot(data,t)),axis=1,keepdims=True)-np.ones(shape=(data.shape[0],1))
+
+res = minimize(fun, np.random.rand(len(var)*2), method='SLSQP',  constraints=cons)
+
+
+print fun(res.x)/279
 #print (cost1*row1+cost2*row2)/13826
 # p=makeCart(rawData.loc[rindexes,var].as_matrix(),rawData.loc[rindexes,['high','medium','low']].as_matrix(),var)
 
@@ -114,6 +135,4 @@ pred.to_csv('output\\highnalysis.csv', sep=',')
 # timeTaken = time.time() - start_time
 # print "time taken is"
 # print timeTaken
-
-
 
